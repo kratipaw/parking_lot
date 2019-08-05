2b334df04e62dc0a13db932e9253e090d7652b3f
kratipaw <kratipaw3@gmail.com>
2019-08-05 20:01:58 +0530

    adding project skeleton files

---
+++ src/main/java/com/parkingLot/exceptions/InvalidCommand.java
@@ -0,0 +1,8 @@
+package main.java.com.parkingLot.exceptions;
+
+public class InvalidCommand extends Exception{
+
+    public InvalidCommand(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/parkingLot/exceptions/InvalidCommandArguments.java
@@ -0,0 +1,8 @@
+package main.java.com.parkingLot.exceptions;
+
+public class InvalidCommandArguments extends Exception{
+
+    public InvalidCommandArguments(String message){
+        super(message);
+    }
+}
