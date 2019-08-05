e8f218de0fe26b9ee9b16b056682836d6cf654a6
kratipaw <kratipaw3@gmail.com>
2019-08-05 19:55:35 +0530

    adding project skeleton files

---
+++ src/main/java/com/parkingLot/constants/Commands.java
@@ -0,0 +1,4 @@
+package main.java.com.parkingLot.constants;
+
+public enum Commands {
+}
---
+++ src/main/java/com/parkingLot/constants/Messages.java
@@ -0,0 +1,7 @@
+package main.java.com.parkingLot.constants;
+
+public class Messages {
+
+    public static String NOT_FOUND = "Not found";
+    public static String PARKING_LOT_FULL = "Sorry, parking lot is full";
+}
