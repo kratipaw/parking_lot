47aee14e79c050414ec2d0d19d1644dff0d78d80
kratipaw <kratipaw3@gmail.com>
2019-08-14 19:23:01 +0530

    Implemeted Leave command and its JUnit test cases

--- src/main/java/com/parkingLot/commands/LeaveCommand.java
+++ src/main/java/com/parkingLot/commands/LeaveCommand.java
@@ -9,12 +9,15 @@ public class LeaveCommand extends BaseCommand{
     @Override
     protected boolean isValidCommand(String[] cmdTokens) {
 
-        if(cmdTokens.length == 2){
-            spotId = Integer.parseInt(cmdTokens[1]);
-            return true;
-        }
+        if (cmdTokens.length != 2)
+            return false;
 
-        return false;
+        if(cmdTokens[1] == null || cmdTokens[1].isEmpty())
+            return false;
+
+        spotId = Integer.parseInt(cmdTokens[1]);
+
+        return spotId >= 1;
     }
 
     @Override
--- src/test/java/com/parkingLot/commands/LeaveCommandTest.java
+++ src/test/java/com/parkingLot/commands/LeaveCommandTest.java
@@ -1,5 +1,9 @@
 package com.parkingLot.commands;
 
+import com.parkingLot.ParkingLot;
+import com.parkingLot.constants.Messages;
+import com.parkingLot.constants.VehicleType;
+import com.parkingLot.vehicle.Car;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,26 +12,86 @@ public class LeaveCommandTest {
 
     @Test
     public void invalidNumberOfCmdTokens(){
-
+        String[] cmdTokens = new String[1];
+        cmdTokens[0] = "leave";
+        LeaveCommand cmd = new LeaveCommand();
+        boolean result = cmd.isValidCommand(cmdTokens);
+        assertFalse(result);
     }
 
     @Test
-    public void invalidCmdTokens(){
+    public void nullSpotId(){
+        String[] cmdTokens = new String[2];
+        cmdTokens[0] = "leave";
+        cmdTokens[1] = null;
+        LeaveCommand cmd = new LeaveCommand();
+        boolean result = cmd.isValidCommand(cmdTokens);
+        assertFalse(result);
+    }
 
+    @Test
+    public void  invalidOrNegativeSpotId(){
+        String[] cmdTokens = new String[2];
+        cmdTokens[0] = "leave";
+        cmdTokens[1] = "-1";
+        LeaveCommand cmd = new LeaveCommand();
+        boolean result = cmd.isValidCommand(cmdTokens);
+        assertFalse(result);
+        cmdTokens[1] = "0";
+        boolean resultZeroSpotId = cmd.isValidCommand(cmdTokens);
+        assertFalse(resultZeroSpotId);
     }
 
     @Test
     public void validCmdTokens(){
+        String[] cmdTokens = new String[2];
+        cmdTokens[0] = "leave";
+        cmdTokens[1] = "1";
+        try {
+            LeaveCommand cmd = new LeaveCommand();
+            boolean result =  cmd.isValidCommand(cmdTokens);
+            assertTrue(result);
+        } catch (Exception e) {
+            fail();
+        }
 
     }
 
     @Test
     public void invalidExecute(){
-
+        String[] cmdTokens = new String[2];
+        cmdTokens[0] = "leave";
+        cmdTokens[1] = "2";
+        try {
+            ParkingLot.initializeParkingLot(2);
+            ParkingLot.parkCar(new Car(VehicleType.CAR, "Reg-Num-1", "blue"));
+            LeaveCommand cmd = new LeaveCommand();
+            boolean result =  cmd.isValidCommand(cmdTokens);
+            assertTrue(result);
+            String resultStr = cmd.executeCommand();
+            assertEquals(Messages.PARKING_SPOT_EMPTY , resultStr);
+            ParkingLot.destroyParkingLotInstance();
+        } catch (Exception e) {
+            fail();
+        }
     }
 
     @Test
     public void validExecute(){
-
+        String[] cmdTokens = new String[2];
+        cmdTokens[0] = "leave";
+        cmdTokens[1] = "1";
+        try {
+            ParkingLot.initializeParkingLot(1);
+            ParkingLot.parkCar(new Car(VehicleType.CAR, "Reg-Num-1", "blue"));
+            LeaveCommand cmd = new LeaveCommand();
+            boolean result =  cmd.isValidCommand(cmdTokens);
+            assertTrue(result);
+            String resultStr = cmd.executeCommand();
+            assertEquals(Messages.PARKING_SPOT + "1 is free", resultStr);
+            ParkingLot.destroyParkingLotInstance();
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
