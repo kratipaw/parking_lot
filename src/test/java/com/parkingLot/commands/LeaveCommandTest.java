bb56216b466d2494a9d87f0088cfd1f494223a1e
kratipaw <kratipaw3@gmail.com>
2019-08-14 14:14:14 +0530

    Added Skeleton of JUnit Test for Leave command

---
+++ src/test/java/com/parkingLot/commands/LeaveCommandTest.java
@@ -0,0 +1,33 @@
+package com.parkingLot.commands;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeaveCommandTest {
+
+    @Test
+    public void invalidNumberOfCmdTokens(){
+
+    }
+
+    @Test
+    public void invalidCmdTokens(){
+
+    }
+
+    @Test
+    public void validCmdTokens(){
+
+    }
+
+    @Test
+    public void invalidExecute(){
+
+    }
+
+    @Test
+    public void validExecute(){
+
+    }
+}
